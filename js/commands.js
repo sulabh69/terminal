e99306456ffa51296633b823d8543b3db2fbc784
sulabh69 <145089427+sulabh69@users.noreply.github.com>
2024-09-21 21:07:03 +0530

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,77 @@
+var twitter = "twitter.png";
+var password = "sulabh";
+var linkedin = "linkedin.png";
+var instagram = "https://www.instagram.com/aayushuknow/";
+var github = "https://github.com/sulabh69";
+var email = 'mailto:antoniyomonatan99@gamil.com';
+
+whois = [
+  "<br>",
+  "Hey, I'm aayush kumawat",
+  "I'm a software engineer ",
+  "After graduating with a Bachelor's in Computer Science, I worked professionally",
+  "as a software engineer building enterprise web applications for Fortune 500 companies.",
+  "the next generation of developers and help them navigate the crazy world that is software", "engineering & computer science.",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of â€œWho am I?â€ is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/AAyushkumawat' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/AAyushumawat' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/AAyushkumawat' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/AAyushkumawat' + "</a>",
+  'email         <a href="' + email + '" target="_blank">email/AAyushkumawat' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Aayush kumawat?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Aayushkumawat(AK) Not A Corporation. All knights reserved.</span>',
+  "         ___   ____",
+  "       /' --;^/ ,-_\\     \\ | /       ",
+  "      / / --o\\ o-\\ \\\\   --(_)--   ",
+  "     /-/-/|o|-|\\-\\\\|\\\\   / | \\   ",
+  "      '`  ` |-|   `` '           _       ___   ___   _ _ ___ _   _  ",
+  "            |-|                 / \      / \\ \  / / | | / ___|| | | |",
+  "            |-|O               / _ \    / _ \\ V /| | | \ ___ \ | |_| |",
+  "            |-(\\,__           / ___ \  / ___ \| | | |_| |___) |  _  |",
+  "         ...|-|\\--,\\_....    /_/   \_\/_/   \_\_|     \___/|____/|_| |_|",
+  "      ,;;;;;;;;;;;;;;;;;;;;;;;;,.   ",
+  "~~,;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
+
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
